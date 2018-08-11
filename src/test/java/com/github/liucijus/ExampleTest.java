49738338f4638538183cec3454a31f5e9f66c259
Vaidas Pilkauskas <vaidasp@wix.com>
2018-08-11 14:22:10 +0300

    Add sample failing junit 5 test to validate if engine works

---
+++ src/test/java/com/github/liucijus/ExampleTest.java
@@ -0,0 +1,13 @@
+package com.github.liucijus;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ExampleTest {
+    @Test
+    void verifyWorks() {
+        int expected = 1;
+        int actual = 2;
+        Assertions.assertEquals(expected, actual);
+    }
+}
